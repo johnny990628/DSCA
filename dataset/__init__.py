88bd333df6d70458891e32f626937a197fcc3c1f
Johnny Li <80705123+johnny990628@users.noreply.github.com>
2025-08-06 11:57:55 +0800

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,3 @@
 from .dataset_interface import prepare_dataset
-from .WSIPatchData import SurvLabelTransformer
+from .WSIPatchData import SurvLabelTransformer, WSIDataset
 from .WSIPatchData import WSIPatchDataset, WSIOnePatchDataset
